41ab2166a7c43b4c16fb5e9c9e8b49423e43a1ed
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-05-09 17:22:25 +0900

    [add]シングルトンとして機能させるための抽象クラスとゲーム全体を管理するManagerクラスを作成

---
+++ Assets/Scripts/Nakajima/System/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+/// <summary>
+/// ゲーム全体を管理するManagerクラス
+/// </summary>
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    #region property
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    public event Action OnGameStart;
+    public event Action OnGamePause;
+    public event Action OnGameEnd;
+    #endregion
+
+    #region unity methods
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
