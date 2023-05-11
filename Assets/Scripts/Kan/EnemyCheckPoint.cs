e0b1b6ad2f9fe6a48509efa88337437b81b923c0
kannkeii <kannkeii2015@gmail.com>
2023-05-11 11:50:15 +0900

    [update]敵に最大HP(MaxHP)と現在HP(hp)情報を追加、敵のHPバーを操作できる関数を作成。敵のHPゲージを作成。

---
+++ Assets/Scripts/Kan/EnemyCheckPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class EnemyCheckPoint : MonoBehaviour
+{
+    public List<GameObject> PointList;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PointList = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddNewPoint()
+    { 
+        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        obj.transform.parent = transform;
+        obj.transform.localPosition = Vector3.zero;
+        PointList.Add(obj);
+    }
+
+    //public override void OnInspectorGUI()
+    //{
+    //    //元のInspector部分を表示
+    //    base.OnInspectorGUI();
+
+    //    //ボタンを表示
+    //    if (GUILayout.Button("Button"))
+    //    {
+    //        Debug.Log("押した!");
+    //    }
+    //}
+}
