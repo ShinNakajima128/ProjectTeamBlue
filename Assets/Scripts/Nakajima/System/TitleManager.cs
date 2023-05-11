1fdbeeefb4e785d74ed7eef59b7da4f9f4fa9ed3
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-05-11 14:35:39 +0900

    [add]タイトル画面の機能を仮作成

---
+++ Assets/Scripts/Nakajima/System/TitleManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// タイトルの機能を管理するManagerクラス
+/// </summary>
+public class TitleManager : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    [Tooltip("ボタンを押して読み込むSceneの名前(仮機能)")]
+    [SerializeField]
+    private string _loadSceneName = "";
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    /// <summary>
+    /// ステージ選択画面を読み込む
+    /// </summary>
+    public void LoadStageSelectScene()
+    {
+        SceneManager.LoadScene(_loadSceneName);
+    }
+    #endregion
+
+    #region private method
+    #endregion
+}
