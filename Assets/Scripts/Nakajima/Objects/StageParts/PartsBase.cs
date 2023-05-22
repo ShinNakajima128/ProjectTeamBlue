b682f7f0ad8224d311cf28c667ae167242d4bda0
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-05-22 17:27:16 +0900

    [update]パーツの各向きの画像をセットする項目を追加

--- Assets/Scripts/Nakajima/Objects/StageParts/PartsBase.cs
+++ Assets/Scripts/Nakajima/Objects/StageParts/PartsBase.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 /// <summary>
 /// ステージを構成する各パーツの基底クラス。オブジェクトへのアタッチは不可
@@ -10,7 +12,17 @@ public abstract class PartsBase : MonoBehaviour
     #region property
     public string PartsName => _partsName;
     public PartsType PartsType => _partsType;
-    public Texture2D PartsTexture => _partsTexture;
+    public Texture2D[] PartsTextures => _partsTextures;
+    public Texture2D CurrentDirTexture => GetTextureByDirection(CurrentDirType);
+    public DirectionType CurrentDirType 
+    {
+        get => _currentDirType;
+        set
+        {
+            _currentDirType = value;
+            RotateByDirection(_currentDirType);
+        }
+    }
     #endregion
 
     #region serialize
@@ -22,12 +34,17 @@ public abstract class PartsBase : MonoBehaviour
     [SerializeField]
     protected PartsType _partsType = default;
 
+    [Tooltip("パーツの向き")]
+    [SerializeField]
+    private DirectionType _currentDirType = DirectionType.North;
+
     [Tooltip("パーツの画像")]
     [SerializeField]
-    protected Texture2D _partsTexture = default;
+    protected Texture2D[] _partsTextures = default;
     #endregion
 
     #region private
+    
     #endregion
 
     #region Constant
@@ -40,9 +57,61 @@ public abstract class PartsBase : MonoBehaviour
     #endregion
 
     #region public method
+    /// <summary>
+    /// 方向毎のテクスチャを取得する
+    /// </summary>
+    /// <param name="type">現在のパーツの向き</param>
+    public Texture2D GetTextureByDirection(DirectionType type)
+    {
+        Texture2D current = null;
+
+        switch (type)
+        {
+            case DirectionType.North:
+                current = _partsTextures[0];
+                break;
+            case DirectionType.East:
+                current = _partsTextures[1];
+                break;
+            case DirectionType.Sorth:
+                current = _partsTextures[2];
+                break;
+            case DirectionType.West:
+                current = _partsTextures[3];
+                break;
+            default:
+                break;
+        }
+
+        return current;
+    }
     #endregion
 
     #region private method
+    /// <summary>
+    /// 向きによってオブジェクトを回転させる
+    /// </summary>
+    /// <param name="type"></param>
+    private void RotateByDirection(DirectionType type)
+    {
+        switch (type)
+        {
+            case DirectionType.North:
+                transform.localRotation = new Quaternion(0, 0, 0, 0);
+                break;
+            case DirectionType.East:
+                transform.localRotation = new Quaternion(0, 90, 0, 0);
+                break;
+            case DirectionType.Sorth:
+                transform.localRotation = new Quaternion(0, 180, 0, 0);
+                break;
+            case DirectionType.West:
+                transform.localRotation = new Quaternion(0, 270, 0, 0);
+                break;
+            default:
+                break;
+        }
+    }
     #endregion
 }
 
@@ -56,4 +125,12 @@ public enum PartsType
     Corridor,
     /// <summary>部屋</summary>
     Room
+}
+
+public enum DirectionType
+{
+    North,
+    East,
+    Sorth,
+    West
 }
