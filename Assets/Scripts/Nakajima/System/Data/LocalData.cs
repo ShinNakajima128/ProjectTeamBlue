8edc522b54f6b3d0a8c4a99665230333124790c3
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-05-26 13:36:20 +0900

    [update]ステージのjsonデータからステージ編集画面を復元する処理を追加

--- Assets/Scripts/Nakajima/System/Data/LocalData.cs
+++ Assets/Scripts/Nakajima/System/Data/LocalData.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.IO;
-class LocalData
+//参考元：https://marumaro7.hatenablog.com/entry/litjsonjagged
+using LitJson;
+using System;
+
+public class LocalData
 {
     /// <summary>
     /// ローカルファイルにデータを保存する
@@ -14,6 +18,28 @@ class LocalData
 
         StreamWriter writer;
         var json = JsonUtility.ToJson(data);
+        using (writer = new StreamWriter(Application.dataPath + "/" + file, false))
+        {
+            Debug.Log(Application.dataPath + "/" + file);
+            writer.Write(json);
+            writer.Flush();
+            writer.Close();
+        }
+    }
+
+    /// <summary>
+    /// 多次元配列を含むデータをローカルファイルに保存する
+    /// </summary>
+    /// <typeparam name="T"> データの型 </typeparam>
+    /// <param name="file"> 保存先のファイル名 </param>
+    /// <param name="data"> 現在のデータ </param>
+    public static void SaveMultidimArray<T>(string file, T data)
+    {
+        Debug.Log("Save");
+
+        StreamWriter writer;
+        var json = JsonMapper.ToJson(data);
+
         using (writer = new StreamWriter(Application.dataPath + "/" + file, false))
         {
             Debug.Log(Application.dataPath + "/" + file);
@@ -57,9 +83,43 @@ class LocalData
             return default;
         }
     }
-    static void Create<T>(T data)
-    {
 
+    /// <summary>
+    /// 多次元配列を含むデータをローカルファイルから取得する
+    /// </summary>
+    /// <param name="filePath"> 保存先のファイル名 </param>
+
+    public static T LoadMultidimArray<T>(string filePath)
+    {
+        string datastr;
+        StreamReader reader;
+        try
+        {
+            using (reader = new StreamReader(filePath))
+            {
+                datastr = reader.ReadToEnd();
+                reader.Close();
+            }
+
+            Debug.Log(datastr);
+            var data = JsonMapper.ToObject<T>(datastr);
+
+            if (data != null)
+            {
+                Debug.Log(data + "のデータをロードしました");
+
+                return data;
+            }
+            else
+            {
+                return default;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"データを取得できませんでした。ファイルパス：{filePath} {e}");
+            return default;
+        }
     }
 }
 
