8a1f11d987303a16abbdf3c12d0ea13f3c60a3a4
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-06-08 17:42:26 +0900

    [add]リザルト画面のUIを作成中

---
+++ Assets/Scripts/Nakajima/UI/ResultViewer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ResultViewer : MonoBehaviour
+{
+    #region property
+    public static ResultViewer Instance { get; private set; }
+    #endregion
+
+    #region serialize
+    [SerializeField]
+    private Text _mainTargetScoreText;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
